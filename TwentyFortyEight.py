a611f5bffda7dbdb9312e4882090028de593088f
Paul Huang <paul_hzq@hotmail.com>
2017-02-12 20:18:26 -0500

    finish the logic and game

--- TwentyFortyEight.py
+++ TwentyFortyEight.py
@@ -1,12 +1,27 @@
 from tkinter import *
+from logic import *
 
 SIZE = 500
 GRID_LEN = 4
 GRID_PADDING = 10
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
-
+BACKGROUND_COLOR_DICT = {   2:"#eee4da", 4:"#ede0c8", 8:"#f2b179", 16:"#f59563",
+                            32:"#f67c5f", 64:"#f65e3b", 128:"#edcf72", 256:"#edcc61",
+                            512:"#edc850", 1024:"#edc53f", 2048:"#edc22e" }
+CELL_COLOR_DICT = { 2:"#776e65", 4:"#776e65", 8:"#f9f6f2", 16:"#f9f6f2",
+                    32:"#f9f6f2", 64:"#f9f6f2", 128:"#f9f6f2", 256:"#f9f6f2",
+                    512:"#f9f6f2", 1024:"#f9f6f2", 2048:"#f9f6f2" }
 FONT = ("Verdana", 40, "bold")
+KEY_UP_ALT = "\'\\uf700\'"
+KEY_DOWN_ALT = "\'\\uf701\'"
+KEY_LEFT_ALT = "\'\\uf702\'"
+KEY_RIGHT_ALT = "\'\\uf703\'"
+
+KEY_UP = "'w'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
+KEY_RIGHT = "'d'"
 
 class TwentyFortyEight(Frame):
     def __init__(self):
@@ -14,9 +29,14 @@ class TwentyFortyEight(Frame):
 
         self.grid()
         self.master.title('2048')
-        #self.master.bind("<Key>", self.key_down)
+        self.master.bind("<Key>", self.key_press)
+        #commands is a dict where the key is the repr(event.char) and value is the func defined in the logic.py.
+        self.commands = {   KEY_UP: up, KEY_DOWN: down, KEY_LEFT: left, KEY_RIGHT: right,
+                        KEY_UP_ALT: up, KEY_DOWN_ALT: down, KEY_LEFT_ALT: left, KEY_RIGHT_ALT: right }
         self.grid_cells = []
         self.init_grid()
+        self.init_matrix()
+        self.update_grid_cells()
         self.mainloop()
 
     def init_grid(self):
@@ -33,5 +53,35 @@ class TwentyFortyEight(Frame):
                 grid_row.append(t)
 
             self.grid_cells.append(grid_row)
+
+    def init_matrix(self):
+        self.matrix = new_game(4)
+        self.matrix=add_two(self.matrix)
+        self.matrix=add_two(self.matrix)
+
+    def update_grid_cells(self):
+        for i in range(GRID_LEN):
+            for j in range(GRID_LEN):
+                new_number = self.matrix[i][j]
+                if new_number == 0:
+                    self.grid_cells[i][j].configure(text="", bg=BACKGROUND_COLOR_CELL_EMPTY)
+                else:
+                    self.grid_cells[i][j].configure(text=str(new_number), bg=BACKGROUND_COLOR_DICT[new_number], fg=CELL_COLOR_DICT[new_number])
+        self.update_idletasks() #update the geometry
+
+    def key_press(self, event):
+        key = repr(event.char)
+        if key in self.commands:
+            self.matrix,done = self.commands[repr(event.char)](self.matrix)
+            if done:
+                self.matrix = add_two(self.matrix)
+                self.update_grid_cells()
+                done=False
+                if game_state(self.matrix)=='win':
+                    self.grid_cells[1][1].configure(text="You",bg=BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Win!",bg=BACKGROUND_COLOR_CELL_EMPTY)
+                if game_state(self.matrix)=='lose':
+                    self.grid_cells[1][1].configure(text="You",bg=BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Lose!",bg=BACKGROUND_COLOR_CELL_EMPTY)
 
 twentyfortyeight = TwentyFortyEight()
